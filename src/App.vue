fcfb573916182a2e68dcf8f38f3d526f58c332a2
Ochuwa-sophie <sophiecodes@gmail.com>
2021-07-16 21:45:54 +0100

    setup analytics

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,23 @@
 
 <script>
 // import NavbarComponent from "@/components/NavbarComponent.vue";
-// import * as firebase from "firebase/app";
-// import { version } from "../package.json";
+import * as firebase from "firebase/app";
+import { version } from "../package.json";
 
 export default {
   components: {
     // NavbarComponent
+  },
+  metaInfo: {
+    changed(metaInfo) {
+      firebase.analytics().setCurrentScreen(metaInfo.title);
+      firebase.analytics().logEvent({
+        app_name: "web", //this.isPWA() ? "pwa" : "web",
+        screen_name: metaInfo.title,
+        app_version: version
+      });
+    }
   }
-  // metaInfo: {
-  //   changed(metaInfo) {
-  //     firebase.analytics().setCurrentScreen(metaInfo.title);
-  //     firebase.analytics().logEvent({
-  //       app_name: "web", //this.isPWA() ? "pwa" : "web",
-  //       screen_name: metaInfo.title,
-  //       app_version: version
-  //     });
-  //   }
-  // }
 };
 </script>
 
